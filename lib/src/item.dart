79720d2f41df4d8d69d913bd7a565aa8fbc02e20
ATarsa <smb909@gmail.com>
2019-06-10 11:31:07 +0100

    Add Wallet class

---
+++ lib/src/item.dart
@@ -0,0 +1,124 @@
+class Item {
+  int id;
+  String currency;
+  double amount;
+
+  // item constructor
+  Item(this.id, this.currency, this.amount);
+  @override
+  String toString() => '$id $currency $amount';
+
+}
+
+class Wallet {
+  List<Item> _itemsList = [];
+  String _baseCurrency = 'GBP';
+
+  // Constructor
+Wallet(this._itemsList, this._baseCurrency);
+
+  // methods
+  // getters and setters
+  List<Item> get itemsList{
+    return _itemsList;
+  }
+
+  String get baseCurrency{
+    return _baseCurrency;
+  }
+  set baseCurrency(String currency){
+    this._baseCurrency = currency;
+  }
+
+  //  add item to items array
+  Item addItem(String currency, double amount){
+    int id;
+    // create id
+    if (_itemsList.isNotEmpty){
+      id = _itemsList[_itemsList.length-1].id;
+      id += 1;
+    } else {
+      id = 0;
+    }
+    // create new Item object
+    Item newItem = Item(id, currency, amount);
+    // add to array
+    _itemsList.add(newItem);
+    // TODO: add to local storage
+    // return new item
+    return newItem;
+  }
+
+}
+
+
+Item _currentItem;
+
+//var _baseCurrency=  'GBP'; // init with GBP as default; TODO: get base
+////currency from local storage on load
+Map<String, double> _exchangeRates = {};
+
+// Available currencies
+/* list compatible with https://exchangeratesapi.io/
+and foreign exchange rates published by
+the European Central Bank
+https://www.ecb.europa.eu/stats/policy_and_exchange_rates/euro_reference_exchange_rates/html/index.en.html */
+Map<String, String> _currenciesList = {'EUR': 'Euro',
+'USD': 'US dollar',
+'JPY': 'Japanese yen',
+'BGN': 'Bulgarian lev',
+'CZK': 'Czech koruna',
+'DKK': 'Danish krone',
+'GBP': 	'Pound sterling',
+'HUF' :	'Hungarian forint',
+'PLN' :	'Polish zloty',
+'RON' :	'Romanian leu',
+'SEK' :	'Swedish krona',
+'CHF' :	'Swiss franc',
+'ISK' :	'Icelandic krona',
+'NOK' :	'Norwegian krone',
+'HRK' :	'Croatian kuna',
+'RUB' :	'Russian rouble',
+'TRY' :	'Turkish lira',
+'AUD' :	'Australian dollar',
+'BRL': 	'Brazilian real',
+'CAD': 	'Canadian dollar',
+'CNY' :  'Chinese yuan renminbi',
+'HKD'  : 'Hong Kong dollar',
+'IDR'  : 'Indonesian rupiah',
+'ILS' :	'Israeli shekel',
+'INR' :	'Indian rupee',
+'KRW' :'South Korean won',
+'MXN' :	'Mexican peso',
+'MYR' :	'Malaysian ringgit',
+'NZD' :	'New Zealand dollar',
+'PHP' :	'Philippine peso',
+'SGD' :	'Singapore dollar',
+'THB' :	'Thai baht',
+'ZAR': 	'South African rand'};
+
+// Public methods
+
+
+// TODO: delete item
+
+// TODO: update item
+
+// TODO: get data item by id
+
+
+// TODO: clear all data items
+
+
+// TODO: set current item
+
+// TODO: get current item
+
+// TODO: set base currency
+
+// TODO: get base currency
+// TODO:  get available currencies
+// TODO: fetch currencies rates
+// TODO: get exchange rates
+// TODO:  set exchange rate
+// TODO: exchange money
